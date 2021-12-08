8d393ab9c4055e995ff2432e019d8f9498a0cd23
s20192087 <20192087-onokota@hcs.ac.jp>
2021-12-08 11:04:43 +0900

    csv

--- review_csv.py
+++ review_csv.py
@@ -39,7 +39,7 @@ def get_all_reviews(url):
         reviews = amazon_bs.select('.review-text')          #　ページ内の全レビューのテキストを取得
         stars  = amazon_bs.select('a.a-link-normal span.a-icon-alt')
         
-        for j in range(len(reviews)):
+        for j in range(len(stars)):
             article = {
             "text": reviews[j].text.replace("\n", "").replace("\u3000", ""),
             "label": stars[j].text,
@@ -67,10 +67,10 @@ if __name__ == '__main__':
     urls = []
     # スイッチ
     #スマブラ マリオカートDX pokemon剣盾 スプラ
-    urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E5%A4%A7%E4%B9%B1%E9%97%98%E3%82%B9%E3%83%9E%E3%83%83%E3%82%B7%E3%83%A5%E3%83%96%E3%83%A9%E3%82%B6%E3%83%BC%E3%82%BA-SPECIAL-Switch/product-reviews/B07FDW61HX/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
-    urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9E%E3%83%AA%E3%82%AA%E3%82%AB%E3%83%BC%E3%83%888-%E3%83%87%E3%83%A9%E3%83%83%E3%82%AF%E3%82%B9-Switch/product-reviews/B01N12G06K/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
-    urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9D%E3%82%B1%E3%83%83%E3%83%88%E3%83%A2%E3%83%B3%E3%82%B9%E3%82%BF%E3%83%BC-%E3%82%BD%E3%83%BC%E3%83%89-Switch/product-reviews/B07V3KK93X/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
-    urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-Splatoon-2-%E3%82%B9%E3%83%97%E3%83%A9%E3%83%88%E3%82%A5%E3%83%BC%E3%83%B32-Switch/product-reviews/B072J2J26T/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
+    # urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E5%A4%A7%E4%B9%B1%E9%97%98%E3%82%B9%E3%83%9E%E3%83%83%E3%82%B7%E3%83%A5%E3%83%96%E3%83%A9%E3%82%B6%E3%83%BC%E3%82%BA-SPECIAL-Switch/product-reviews/B07FDW61HX/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
+    # urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9E%E3%83%AA%E3%82%AA%E3%82%AB%E3%83%BC%E3%83%888-%E3%83%87%E3%83%A9%E3%83%83%E3%82%AF%E3%82%B9-Switch/product-reviews/B01N12G06K/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
+    # urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9D%E3%82%B1%E3%83%83%E3%83%88%E3%83%A2%E3%83%B3%E3%82%B9%E3%82%BF%E3%83%BC-%E3%82%BD%E3%83%BC%E3%83%89-Switch/product-reviews/B07V3KK93X/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
+    # urls.append('https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-Splatoon-2-%E3%82%B9%E3%83%97%E3%83%A9%E3%83%88%E3%82%A5%E3%83%BC%E3%83%B32-Switch/product-reviews/B072J2J26T/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
     # ps4
     #ペルソナ sekiro BF 仁王2
     urls.append('https://www.amazon.co.jp/%E3%82%A2%E3%83%88%E3%83%A9%E3%82%B9-%E3%83%9A%E3%83%AB%E3%82%BD%E3%83%8A5-PS4/product-reviews/B01F377U84/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     urls.append('https://www.amazon.co.jp/%E3%82%A8%E3%83%AC%E3%82%AF%E3%83%88%E3%83%AD%E3%83%8B%E3%83%83%E3%82%AF%E3%83%BB%E3%82%A2%E3%83%BC%E3%83%84-Battlefield-V-%E3%83%90%E3%83%88%E3%83%AB%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB%E3%83%89V-PS4/product-reviews/B07D85MDDM/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
     urls.append('https://www.amazon.co.jp/%E3%82%B3%E3%83%BC%E3%82%A8%E3%83%BC%E3%83%86%E3%82%AF%E3%83%A2%E3%82%B2%E3%83%BC%E3%83%A0%E3%82%B9-%E4%BB%81%E7%8E%8B2/product-reviews/B07ZSZVL9S/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
 
-    for k, url in enumerate(urls):
+    for k, url in enumerate(urls, start=4):
         review_list = []
         review_url = url.replace('dp', 'product-reviews')
         review_list = get_all_reviews(review_url)
