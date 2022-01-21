7ed3b465cad5eef6c79e2a45e99011d27a391ebf
s20192087 <20192087-onokota@hcs.ac.jp>
2022-01-21 11:25:16 +0900

    修正

--- sorce/service_oono.py
+++ sorce/service_oono.py
@@ -12,8 +12,8 @@ import sqlite3
 # ロボット扱いにされた場合、使う
 # testurl = "z:/UserProfile/s20193085/Desktop/data/check/Amazon.co.jp_ スーパーマリオ 3Dワールド + フューリーワールド_オンラインコード版 _ ゲーム.html"
 # ロボット扱いにされてない場合、使う
-# testurl = "https://www.amazon.co.jp/%E3%83%90%E3%83%B3%E3%83%80%E3%82%A4%E3%83%8A%E3%83%A0%E3%82%B3%E3%82%A8%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%86%E3%82%A4%E3%83%B3%E3%83%A1%E3%83%B3%E3%83%88-%E3%80%90PS4%E3%80%91%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC-%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%AA%E3%83%83%E3%83%88%E3%82%B7%E3%83%BC%E3%82%BA%E3%83%B3%E3%80%90%E6%97%A9%E6%9C%9F%E8%B3%BC%E5%85%A5%E7%89%B9%E5%85%B8%E3%80%91%E8%A1%A3%E8%A3%85DLC%E3%80%8E%E6%9A%81%E3%81%AE%E3%82%86%E3%81%8B%E3%81%9F%E3%80%8F%E3%81%8C%E5%85%A5%E6%89%8B%E3%81%A7%E3%81%8D%E3%82%8B%E3%83%97%E3%83%AD%E3%83%80%E3%82%AF%E3%83%88%E3%82%B3%E3%83%BC%E3%83%89-%E5%B0%81%E5%85%A5/dp/B08W5S54P1/ref=sr_1_7?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC&qid=1639620405&sr=8-7"
-testurl = "https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9E%E3%83%AA%E3%82%AA%E3%83%91%E3%83%BC%E3%83%86%E3%82%A3-%E3%82%B9%E3%83%BC%E3%83%91%E3%83%BC%E3%82%B9%E3%82%BF%E3%83%BC%E3%82%BA-Switch/dp/B097BL85Y7/ref=lp_5121199051_1_4?th=1"
+testurl = "https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9E%E3%83%AA%E3%82%AA%E3%83%91%E3%83%BC%E3%83%86%E3%82%A3-%E3%82%B9%E3%83%BC%E3%83%91%E3%83%BC%E3%82%B9%E3%82%BF%E3%83%BC%E3%82%BA-Switch/dp/B097BL85Y7/ref=lp_5121199051_1_4"
+# testurl = "https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%9E%E3%83%AA%E3%82%AA%E3%83%91%E3%83%BC%E3%83%86%E3%82%A3-%E3%82%B9%E3%83%BC%E3%83%91%E3%83%BC%E3%82%B9%E3%82%BF%E3%83%BC%E3%82%BA-Switch/dp/B097BL85Y7/ref=lp_5121199051_1_4?th=1"
 
 # 商品のジャンル次第で分析するファイルを選ぶ
 def analysischoise(category,allreview):
@@ -108,13 +108,14 @@ def reviewSelection(url):
 
 #テスト用受け渡し※後に消せ
 if __name__ == '__main__':
+    temurl = ''
     db = sqlite3.connect("kekka.db")
     
     sql = 'SELECT amazon_url FROM kekka WHERE amazon_url = ?'
     cur = db.execute(sql,(testurl,))
     data = cur.fetchall()
-    temurl = ''.join(data[0])
-    print(temurl)
+    if len(data) != 0:
+        temurl = ''.join(data[0])
     db.close()
 
     if testurl != temurl:
@@ -141,7 +142,6 @@ if __name__ == '__main__':
     
         sql1 = 'CREATE TABLE IF NOT EXISTS kekka (amazon_url TEXT primary key,product_name TEXT, img TEXT,positaitle1 TEXT,positive1 TEXT,positaitle2 TEXT,positive2 TEXT,positaitle3 TEXT,positive3 TEXT,negataitle1 TEXT,negative1 TEXT,negataitle2 TEXT,negative2 TEXT,negataitle3 TEXT,negative3 TEXT,posicount INTEGER,negacount INTEGER)'
         db.execute(sql1)
-        db.close()
     
         sql2 = 'INSERT INTO kekka(amazon_url,product_name, img,positaitle1,positive1,positaitle2,positive2,positaitle3,positive3,negataitle1,negative1,negataitle2,negative2,negataitle3,negative3,posicount,negacount) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
         data = (testurl,selection.name,selection.img,selection.posititle[0],selection.positive[0],selection.posititle[1],selection.positive[1],selection.posititle[2],selection.positive[2],selection.negatitle[0],selection.negative[0],selection.negatitle[1],selection.negative[1],selection.negatitle[2],selection.negative[2],selection.posicount,selection.negacount)
