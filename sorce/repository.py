1ef71b65cdaae3f8879c1fa2c21147952788f936
s20192087 <20192087-onokota@hcs.ac.jp>
2022-02-16 14:45:05 +0900

    新しいDB

--- sorce/repository.py
+++ sorce/repository.py
@@ -1,6 +1,7 @@
 from distutils.log import error
 import sqlite3
 import resultData
+import re
 
 testurl = "https://www.amazon.co.jp/%E3%83%90%E3%83%B3%E3%83%80%E3%82%A4%E3%83%8A%E3%83%A0%E3%82%B3%E3%82%A8%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%86%E3%82%A4%E3%83%B3%E3%83%A1%E3%83%B3%E3%83%88-%E3%80%90PS4%E3%80%91%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC-%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%AA%E3%83%83%E3%83%88%E3%82%B7%E3%83%BC%E3%82%BA%E3%83%B3%E3%80%90%E6%97%A9%E6%9C%9F%E8%B3%BC%E5%85%A5%E7%89%B9%E5%85%B8%E3%80%91%E8%A1%A3%E8%A3%85DLC%E3%80%8E%E6%9A%81%E3%81%AE%E3%82%86%E3%81%8B%E3%81%9F%E3%80%8F%E3%81%8C%E5%85%A5%E6%89%8B%E3%81%A7%E3%81%8D%E3%82%8B%E3%83%97%E3%83%AD%E3%83%80%E3%82%AF%E3%83%88%E3%82%B3%E3%83%BC%E3%83%89-%E5%B0%81%E5%85%A5/dp/B08W5S54P1/ref=sr_1_7?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC&qid=1639620405&sr=8-7"
 testtitle = ""
@@ -21,12 +22,13 @@ def selectdb(url):
     db = sqlite3.connect("kekka.db")
     temurl = ''
     
-    sql = 'SELECT amazon_url FROM kekka WHERE amazon_url = ?'
-    cur = db.execute(sql,(url,))
+    url_a = re.search('[^0-9A-Z]([0-9A-Z]{10})([^0-9A-Z]|$)', url).group().replace("/","")
+    print(url_a)
+    sql = 'SELECT asin_code FROM kekka WHERE asin_code = ?'
+    cur = db.execute(sql,(url_a,))
     data = cur.fetchall()
     print(data)
     print(1)
-    
     # sql実行結果の件数が0件ではないか確認
     if len(data) != 0:
         temurl = ''.join(data[0])
@@ -35,10 +37,10 @@ def selectdb(url):
     db.close()
     
     # 入力されたurlとsql検索結果のurlが一致したかどうか確認
-    if (url == temurl):
+    if (url_a == temurl):
         db = sqlite3.connect("kekka.db")
         # 入力されたurlの全情報をsqlから取得する
-        sql = 'SELECT * FROM kekka WHERE amazon_url = ?'
+        sql = 'SELECT * FROM kekka WHERE asin_code = ?'
         cur = db.execute(sql,(temurl,))
         kekka = cur.fetchall()
         work = kekka[0]
@@ -74,11 +76,12 @@ def insertdb(resultData):
     db = sqlite3.connect("kekka.db",              #ファイル名
         isolation_level=None)
     
-    sql1 = 'CREATE TABLE IF NOT EXISTS kekka (amazon_url TEXT primary key,product_name TEXT, img TEXT,positaitle1 TEXT,positive1 TEXT,positaitle2 TEXT,positive2 TEXT,positaitle3 TEXT,positive3 TEXT,negataitle1 TEXT,negative1 TEXT,negataitle2 TEXT,negative2 TEXT,negataitle3 TEXT,negative3 TEXT,posicount INTEGER,negacount INTEGER)'
+    sql1 = 'CREATE TABLE IF NOT EXISTS kekka (asin_code TEXT primary key,product_name TEXT, img TEXT,positaitle1 TEXT,positive1 TEXT,positaitle2 TEXT,positive2 TEXT,positaitle3 TEXT,positive3 TEXT,negataitle1 TEXT,negative1 TEXT,negataitle2 TEXT,negative2 TEXT,negataitle3 TEXT,negative3 TEXT,posicount INTEGER,negacount INTEGER)'
     db.execute(sql1)
     
-    sql2 = 'INSERT INTO kekka(amazon_url,product_name, img,positaitle1,positive1,positaitle2,positive2,positaitle3,positive3,negataitle1,negative1,negataitle2,negative2,negataitle3,negative3,posicount,negacount) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
-    data = (resultData.url,resultData.name,resultData.img,resultData.posititle[0],resultData.positive[0],resultData.posititle[1],resultData.positive[1],resultData.posititle[2],resultData.positive[2],resultData.negatitle[0],resultData.negative[0],resultData.negatitle[1],resultData.negative[1],resultData.negatitle[2],resultData.negative[2],resultData.posicount,resultData.negacount)
+    url_a = re.search('[^0-9A-Z]([0-9A-Z]{10})([^0-9A-Z]|$)', resultData.url).group().replace("/","")
+    sql2 = 'INSERT INTO kekka(asin_code,product_name, img,positaitle1,positive1,positaitle2,positive2,positaitle3,positive3,negataitle1,negative1,negataitle2,negative2,negataitle3,negative3,posicount,negacount) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+    data = (url_a,resultData.name,resultData.img,resultData.posititle[0],resultData.positive[0],resultData.posititle[1],resultData.positive[1],resultData.posititle[2],resultData.positive[2],resultData.negatitle[0],resultData.negative[0],resultData.negatitle[1],resultData.negative[1],resultData.negatitle[2],resultData.negative[2],resultData.posicount,resultData.negacount)
     db.commit()
     db.execute(sql2,data)     #sql文を実行
     db.close()          #データベースを閉じる
